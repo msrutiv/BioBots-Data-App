f786a2ee3dc4f15f81cd2c04a44c568bd144c749
msrutiv <msrutiv@gmail.com>
2016-08-09 22:02:17 -0400

    cleaned up UI and code

--- routes/getdata.js
+++ routes/getdata.js
@@ -12,63 +12,56 @@ router.post('/specific', getSpecificEntries);
 
 
 function getEntry(req, res, next) {
+	//grab a single entry
 	var entrynum = req.body.entrynum;
-	console.log(entrynum);
 	var group = Math.floor(entrynum/100);
-	//var data = {};
 	var query = database.ref("9-8-116"+"/Entry"+entrynum.toString());
 	query.once('value', function(snapshot) {
 		var entry = snapshot.val();
-		res.render('results', { title: 'Your Worst Nightmare', count: 1, entry: [entry], analysis: []}); 
+		res.render('results', { title: 'Your Worst Nightmare', count: 1, entry: [entry], analysis: null}); 
 	});
 }
 
 function getSpecificEntries(req, res, next) {
+	//grab all entries with specific parameter, currently wellplate #
+	var parameter = parseInt(req.body.platenum);
+	var count = 0;
+	var entries = [];
+	var d_live = 0; var d_dead = 0; //delta
+	var m_live = 0; var m_dead = 0; //mean
+	var v_live = 0; var v_dead = 0; //variance
+	var s_live = 0; var s_dead = 0; //std dev
+	var max_live = 0;
+	var min_live = 100;
+	var query = database.ref("9-8-116").orderByChild('print_info/wellplate').equalTo(parameter);
+	query.on("child_added", function(snapshot){
+      count++;
+      var entry = snapshot.val();
+      entries.push(entry);
 
-			var parameter = parseInt(req.body.platenum);
-			var count = 0;
-			var entries = [];
-			var d_live = 0; var d_dead = 0; //delta
-			var m_live = 0; var m_dead = 0; //mean
-			var v_live = 0; var v_dead = 0; //variance
-			var s_live = 0; var s_dead = 0; //std dev
-			var max_live = 0;
-			var min_live = 100;
-			var query = database.ref("9-8-116").orderByChild('print_info/wellplate').equalTo(parameter);
-			query.on("child_added", function(snapshot){
-	          count++;
-	          var entry = snapshot.val();
-	          entries.push(entry);
+      //summary analysis calculations
+      var live = entry.print_data.livePercent;
+      var dead = entry.print_data.deadPercent;
+      if (live > max_live) max_live = live;
+      if (live < min_live) min_live = live;
+      
+      d_live = live - m_live;
+      m_live += d_live/count;
+      v_live += d_live*(live - m_live);
 
-	          //analysis calculations
-	          var live = entry.print_data.livePercent;
-	          var dead = entry.print_data.deadPercent;
-	          if (live > max_live) max_live = live;
-	          if (live < min_live) min_live = live;
-	          
-	          d_live = live - m_live;
-	          m_live += d_live/count;
-	          v_live += d_live*(live - m_live);
+      d_dead = dead - m_dead;
+      m_dead += d_dead/count;
+      v_dead += d_dead*(dead - m_dead);
 
-	          d_dead = dead - m_dead;
-	          m_dead += d_dead/count;
-	          v_dead += d_dead*(dead - m_dead);
-
-	        }); 
-			setTimeout( function(){
-				s_live = Math.sqrt(v_live);
-				s_dead = Math.sqrt(v_dead);
-				var analysis = { avg_live: m_live, avg_dead: m_dead, stdv_live: s_live, stdv_dead: s_dead, min_live: min_live, max_live: max_live};
-				res.render('results', { title: 'Your Best Nightmare', count: count, entry: entries, analysis: analysis});
-			}, 3500);
-			
-			
-				//console.log(count);
-			
-
-		//res.send("cooldude");	 
+    }); 
+    //hacky way to get around asynch calls to DB
+	setTimeout( function(){
+		s_live = Math.sqrt(v_live/(count-1));
+		s_dead = Math.sqrt(v_dead/(count-1));
+		var analysis = { avg_live: m_live.toPrecision(4), avg_dead: m_dead.toPrecision(4), 
+			stdv_live: s_live.toPrecision(4), stdv_dead: s_dead.toPrecision(4), min_live: min_live, max_live: max_live};
+		res.render('results', { title: 'Your Best Nightmare', count: count, entry: entries, analysis: analysis});
+	}, 3500);
 }
 
 module.exports = router;
-
-//query just alive percentage
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var multer = require('multer');
+var firebase = require('firebase');
 
 var today = new Date();
 var day = today.getDate();
@@ -9,6 +10,7 @@ var month = parseInt(today.getMonth()) + 1;
 var year = today.getYear().toString().slice(1);
 var date = day + '-' + month.toString() + '-' + year;
 
+//File upload set up
 var storage  = multer.diskStorage({
 	destination: function(req, file, callback) {
 
@@ -16,55 +18,49 @@ var storage  = multer.diskStorage({
 	},
 
 	filename: function(req, file, callback) {
-
-	callback(null, file.originalname); 
-}
+		callback(null, file.originalname);
+	}
 });
-
 var upload = multer({storage: storage});
 
-var firebase = require('firebase');
 
+//Firebase set up
 var config = {
 	apiKey: "AIzaSyDm4wV6CqAeIy9-KZW1v445Loz-6hN2GGQ",
 	databaseURL: "https://biobots-data-app.firebaseio.com",
 };
-
 firebase.initializeApp(config);
 var database = firebase.database();
 
-
-
-router.post('/upload', upload.single('dataFile'), uploadData);
-
-function uploadData(req, res) {
-	console.log("look at me I'm here");
- 	var filepath = req.file.path;
- 	var filename = req.file.filename.toString();
- 	var name = filename.split(".");
- 	var data = "";
- 	var data_in = fs.readFileSync(filepath, 'utf8', function(error, data) {
- 		if(error) {
- 			return console.log(error);
- 		}
- 		else console.log(data);
- 	});
- 	
- 	try { data = JSON.parse(data_in); }
- 	catch(e) { alert(e); }
-
- 	for (var i=0; i < data.length; i++) {
-		database.ref(name[0]+'_'+date+'/Entry'+i.toString()).set(data[i]);
-	}	
-	res.render("index", {upload: "Upload Complete!"});
-}
-
-/* GET home page. */
+//Router Calls
 router.get('/', function(req, res, next) {
 	res.render('index', {upload: ""});
 });
 
+router.post('/upload', upload.single('dataFile'), uploadData);
 
+function uploadData(req, res) {
+	//upload file then upload data to firebase database
+	// console.log("look at me I'm here");
+	var filepath = req.file.path;
+	var filename = req.file.filename.toString();
+	var name = filename.split(".");
+	var data = "";
+	var data_in = fs.readFileSync(filepath, 'utf8', function(error, data) {
+		if(error) {
+			return console.log(error);
+		}
+		else console.log(data);
+	});
+
+	try { data = JSON.parse(data_in); }
+	catch(e) { alert(e); }
+
+	for (var i=0; i < data.length; i++) {
+		database.ref(name[0]+'_'+date+'/Entry'+i.toString()).set(data[i]);
+	}	
+	res.render("index", {upload: "Upload Completed!"});
+}
 
 
 module.exports = router;
